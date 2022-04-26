9409dc18dc6b243954d553ce79faaa117e8f09e5
Mamadou Moustapha Diallo <sumptring@gmail.com>
2022-04-26 17:18:28 +0000

    Suppression des hard coded articles

--- index.php
+++ index.php
@@ -53,62 +53,6 @@
 			}
 		}
 	?>
-		
-				
-		
-
-		<div class=" col-lg-4">
-				<div class="d-flex flex-column">
-						<div class="image_article"><img src="images/articles/Enseignement-technique.webp" alt="imageAtricle" width="100%"></div>
-						<div>
-								<h5 class="article-title"><a href="article.php">Avis d’appel d’offres du Ministère de l’Enseignement Technique et de la Formation Professionnelle pour l’acquisition d’un véhicule 4X4</a></h5>
-								<p class='infos-sup'><span class="auteur-article">Mamadou Moustapha Diallo</span> - <span class="heure-publication">10/03/2022 15:17</span></p>
-						</div>
-				</div>
-		</div>
-		
-		<div class=" col-lg-4">
-				<div class="d-flex flex-column">
-						<div class="image_article"><img src="images/articles/Colonel-Doumbouya-Syma-Cour-Supreme-prestation-Serment.webp" alt="imageAtricle" width="100%"></div>
-						<div>
-								<h5 class="article-title"><a href="article.php">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa, alias!</a></h5>
-								<p class='infos-sup'><span class="auteur-article">Mamadou Moustapha Diallo</span> - <span class="heure-publication">10/03/2022 15:17</span></p>
-						</div>
-				</div>
-		</div>
-
-		
-		<div class=" col-lg-4">
-				<div class="d-flex flex-column">
-						<div class="image_article"><img src="images/articles/bedroom-7132435__340.webp" alt="imageAtricle" width="100%"></div>
-						<div>
-								<h5 class="article-title"><a href="article.php">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, officiis.</a></h5>
-								<p class='infos-sup'><span class="auteur-article">Mamadou Moustapha Diallo</span> - <span class="heure-publication">10/03/2022 15:17</span></p>
-						</div>
-				</div>
-		</div>
-				
-		<div class=" col-lg-4">
-				<div class="d-flex flex-column">
-						<div class="image_article"><img src="images/articles/ship-7140939__340.webp" alt="imageAtricle" width="100%"></div>
-						<div>
-								<h5 class="article-title"><a href="article.php">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum, vero!</a></h5>
-								<p class='infos-sup'><span class="auteur-article">Mamadou Moustapha Diallo</span> - <span class="heure-publication">10/03/2022 15:17</span></p>
-						</div>
-				</div>
-		</div>
-		
-			
-		<div class=" col-lg-4">
-				<div class="d-flex flex-column">
-						<div class="image-article"><img src="images/articles/PAM-PCG.webp" alt="imageAtricle" width="100%"></div>
-						<div>
-								<h5 class="article-title"><a href="article.php">Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, eveniet!</a></h5>
-								<p class='infos-sup'><span class="auteur-article">Mamadou Moustapha Diallo</span> - <span class="heure-publication">10/03/2022 15:17</span></p>
-						</div>
-				</div>
-		</div>
-
 </div>
 
 <?php require "includes/footer.php" ; ?>
