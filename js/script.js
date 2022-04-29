848421d1dc4a82f57d1ff023af216335388b8999
Mamadou Moustapha Diallo <sumptring@gmail.com>
2022-04-29 08:10:34 +0000

    Making the header responsive

--- js/script.js
+++ js/script.js
@@ -4,4 +4,19 @@ $(document).ready(function(){
   });
 
   $('[data-bs-toggle="tooltip"]').tooltip();
+
+  // autochange le logo du boutton du panneau de navigation
+  $("#menu").on('show.bs.collapse', function(){
+    let btn = $('.btn-nav-action');
+    let fa = $(btn).children();
+    $(btn).removeClass('action-open').addClass('action-close');
+    $(fa).removeClass('fa-bars').addClass('fa-close')
+  });
+
+  $("#menu").on('hidden.bs.collapse', function(){
+    let btn = $('.btn-nav-action');
+    let fa = $(btn).children();
+    $(btn).removeClass('action-close').addClass('action-open');
+    $(fa).removeClass('fa-close').addClass('fa-bars')
+  });
 });
