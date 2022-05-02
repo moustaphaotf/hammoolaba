ecd69108167e503c0ce4a9dfbf370b41d5f644e1
Mamadou Moustapha Diallo <sumptring@gmail.com>
2022-05-02 06:30:47 +0000

    Définition de deux constantes d'administration

--- config.php
+++ config.php
@@ -8,3 +8,12 @@
 	if(defined('MAX_ARTICLES_PER_PAGE') === false){
     define('MAX_ARTICLES_PER_PAGE', 6);
   }
+
+  if(defined('USER') === false){
+    define('USER', 0);
+  }
+
+  if(defined('USER_ADMIN') === false){
+    define('USER_ADMIN', 1);
+  }
+
