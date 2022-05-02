28aedfd0a780278085495228595d9c14a8656595
Mamadou Moustapha Diallo <sumptring@gmail.com>
2022-05-02 09:16:02 +0000

    Lister les catégories d'informations

---
+++ viewcategories.php
@@ -0,0 +1,61 @@
+<?php
+session_start();
+require_once 'config.php';
+
+// gestion des droits
+
+if(!isset($_SESSION['USER_ID'])){
+  header("Location:connexion.php");
+}
+else if($_SESSION['USER_ROLE'] != USER_ADMIN){
+  header("Location:index.php");
+}
+
+$db = new mysqli($hname, $uname, $pword, $dbase);
+$resultcateg = $db->query("SELECT categories.id, categories.name, categories.colortheme, count(articles.id) AS total_articles FROM articles INNER JOIN categories ON articles.cat_id = categories.id GROUP BY categories.id, categories.name, categories.colortheme ORDER BY categories.name");
+
+$fichier_style = "css/viewcategories.css";
+require_once 'includes/header.php';
+?>
+<div class="row">
+  <div class="col  px-4 pt-2">
+    <h2 class="text-center">Topics disponnibles</h2>
+  <?php if($resultcateg->num_rows === 0) : ?>
+    <p>Aucune categorie n'est disponnible, <a href='newcategory.php'>ajoutez-en une</a> !</p>
+  <?php else : ?>
+    <table class="table table-striped align-middle">
+      <thead>
+        <tr class="table-dark">
+          <th>#</th>
+          <th>Nom</th>
+          <th>Articles publiés</th>
+          <th>Couleur de thème</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+          while($rowcat = $resultcateg->fetch_array(MYSQLI_ASSOC)){
+            echo 
+            '<tr>'
+                . "<td>" . $rowcat['id'] . "</td>"
+                . "<td class='category-name'><a href='category.php?id=" . $rowcat['id'] . "'>" . $rowcat['name'] . "</a></td>"
+                . "<td>" . $rowcat['total_articles'] . "</td>"
+                . "<td><span class='badge' style='color:black; background-color:". $rowcat['colortheme'] ."'>" . (isset($rowcat['colortheme']) ? $rowcat['colortheme'] : 'Aucune') . "</span></td>"
+                . "<td>&nbsp"
+                  . '<a class="edit-category" href="editcategory.php?id=' . $rowcat['id'] . '"><i class="fa fa-edit fa-lg"></i></a> &nbsp &nbsp'
+                  . '<a class="delete-category" href="deletecategory.php?id=' . $rowcat['id'] . '" data-article-id="' . $rowcat['id'] . '"><i class="fa fa-trash fa-lg"></i></a>'
+                . "</td>"
+            . '</tr>';
+          }
+        ?>
+      </tbody>
+    </table>
+    <?php 
+    
+    ?>
+  <?php endif ?>
+    
+</div>
+</div>
+<?php require_once 'includes/footer.php'; ?>
