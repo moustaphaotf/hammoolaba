a1410ec407ffb394eef82be85c6deaf12ab466d8
Mamadou Moustapha Diallo <sumptring@gmail.com>
2022-05-03 06:01:25 +0000

    Modification d'un article

---
+++ editcategory.php
@@ -0,0 +1,68 @@
+<?php
+session_start();
+require_once 'config.php';
+
+// gestion des droits
+
+if(!isset($_SESSION['USER_ID'])){
+  header("Location:connexion.php");
+}
+else if($_SESSION['USER_ROLE'] !== USER_ADMIN){
+  header("Location:index.php");
+}
+
+// processing du formulaire de modification
+if(isset($_POST['submit'])){
+  if(!empty($_POST['name'])){
+    $id = htmlentities($_POST['id']);
+    $name = htmlentities($_POST['name']);
+    $color = htmlentities($_POST['color']);
+
+    // on vérifie que le nom choisi n'existe pas déjà
+    $db = new mysqli($hname, $uname, $pword, $dbase);
+    $stmt = $db->prepare("SELECT * FROM categories WHERE name = ? AND id != ?");
+    $stmt->bind_param('si', $name, $id);
+    $stmt->execute();
+
+    $resultcat = $stmt->get_result();
+
+    if($resultcat->num_rows !== 0){
+      $status = false;
+    }
+    else{
+      // on met à jour les données
+      $stmt = $db->prepare("UPDATE categories SET name = ?, colortheme = ? WHERE id = ? LIMIT 1");
+      $stmt->bind_param('ssi', $name, $color, $id);
+      $stmt->execute();
+
+      $status = true; // tout va bien
+    }
+    
+    header("Location:viewcategories.php");
+  }
+}
+// on veut modifier une categorie
+else if (isset($_GET['id'])){
+  // chercher si la categ existe
+  $id = (int)$_GET['id'];
+  
+  $db = new mysqli($hname, $uname, $pword, $dbase);
+
+  $resultcat = $db->query("SELECT * FROM categories WHERE id = " . $id . " LIMIT 1");
+
+  if($resultcat->num_rows === 0){
+    header("Location:404.php");
+  }
+  else{
+    // definition des variables puis charger l'article
+    $categ_to_edit = $resultcat->fetch_array(MYSQLI_ASSOC);
+    $editmode = true;
+    $actionpage = "editcategory.php?id=" . $id;
+
+    require_once "newcategory.php";
+  }
+}
+// essai d'accès à /categorie.php ---> index.php
+else{
+  header("Location:index.php");
+}
--- newcategory.php
+++ newcategory.php
@@ -1,12 +1,25 @@
 <?php 
+require 'config.php';
+session_start();
+
+if(!isset($_SESSION['USER_ID'])){
+  header("Location:connexion.php");
+}
+else if ($_SESSION['USER_ROLE'] != USER_ADMIN){
+  header("Location:index.php");
+}
+
+// définition de la page de processing
+
+if(!isset($editmode)){
+  $actionpage = $_SERVER['PHP_SELF'];
+  $editmode = false;
+}
 
 if(!empty($_POST['name'])){
   $name = htmlentities($_POST['name']);
   $colortheme = htmlentities($_POST['color']);
 
-  //die(print_r($_POST));
-
-  require 'config.php';
   $db = new mysqli($hname, $uname, $pword, $dbase);
 
   $stmt = $db->prepare("SELECT * FROM categories WHERE name = ? LIMIT 1");
@@ -32,7 +45,7 @@ require "includes/header.php";
   <div class="alert alert-dismissible alert-success fade show <?=isset($exists) ?? 'd-none'?>">
     <a class="btn-close" type='button' data-bs-dismiss='alert' aria-label="Close"></a>
     <i class="fa fa-check-circle fa-2x"></i>
-    La catégorie a été ajoutée avec succès
+    <?= ($editmode === true ? 'Les modifications ont été approtés avec success' : 'La catégorie a été ajoutée avec succès') ?>
   </div>
 <?php elseif(isset($status) && $status === false) : ?>
   <div class="alert alert-dismissible alert-warning fade show">
@@ -43,7 +56,7 @@ require "includes/header.php";
 <?php endif ?>
 
 
-<h1 class="text-center">Nouvelle catégorie</h1>
+<h1 class="text-center"><?= ($editmode === true ? 'Modification d\'une catégorie' : 'Nouvelle catégorie') ?></h1>
 <div class="d-flex flex-column align-items-center mt-4">
   <div class="category-explanation d-flex flex-column align-items-center text-center">Nos informations sont catégorisées, ceci permet de faciliter la navigation des utilisateurs.<br>
   Comment ça marche ?<br>
@@ -58,22 +71,23 @@ require "includes/header.php";
   Il n'aura qu'à choisir cette catégories et des articles liés à cette dernière lui seront proposées
 </div>
 </div>
-<form action="" method='post' class="form mt-3">
+<form action="<?=$actionpage?>" method='post' class="form mt-3">
+  <?= ($editmode === true ? "<input type='hidden' name='id' value='$id' />" : "") ?>
   <div class="row">
     <div class="col-sm-6 offset-sm-3 p-3">
       <div>
-        <input type="text" name='name' id='name' class='form-control text-center' placeholder="Nouvelle catégorie ...">
+        <input type="text" name='name' id='name' class='form-control text-center' placeholder="Nouvelle catégorie ..." value="<?=$categ_to_edit['name'] ?? '' ?>">
       </div>
       <div class='my-2 d-flex justify-content-center'>
         <div class="d-flex align-items-center shadow p-1" data-bs-toggle='tooltip' data-bs-placement='right' title="Choisir une couleur de thème">
           <label for="color" >
             <i class="fa fa-pen-fancy pe-3"></i>
           </label>
-          <input type="color" id="color" name="color" class="form-control-color" value='#fabe3C'>
+          <input type="color" id="color" name="color" class="form-control-color" value='<?=$categ_to_edit['colortheme'] ?? 'rgb(22, 44, 90)' ?>'>
         </div>
       </div>
       <div class="d-flex justify-content-center">
-        <button class='btn btn-success'>Ajouter</button>
+        <button class='btn btn-success' name='submit'><?= ($editmode === true ? "Modifier" : "Ajouter") ?></button>
       </div>
     </div>
   </div>
