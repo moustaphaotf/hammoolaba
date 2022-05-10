c624d8da94d50c0fa7b64cf95058e82ec36eb028
Mamadou Moustapha Diallo <sumptring@gmail.com>
2022-05-10 09:08:17 +0000

    Rectification de la mise en forme de la page inscription

--- inscription.php
+++ inscription.php
@@ -2,10 +2,16 @@
 $fichier_style="css/inscription.css";
 require "includes/header.php" ; ?>
 <div class="main d-flex flex-column align-items-center">
-    <h2>Inscription</h2>
     <form action="" method="post" class="form">
+        <h2 class="text-center">Inscription</h2>
+        <div class="input-group">
+            <input placeholder="Nom complet" type="text" id="name" name="name" required class="form-control">
+        </div>
         <div class="input-group">
             <input placeholder="Nom d'utilisateur" type="text" id="username" name="username" required class="form-control">
+        </div>
+        <div class="input-group">
+            <input placeholder="Adresse email" type="text" id="email" name="email" required class="form-control">
         </div>
         <div class="input-group password">
             <input placeholder="Mot de passe" type="password" id="password1" name="password1" required class="form-control">
@@ -14,8 +20,10 @@ require "includes/header.php" ; ?>
             <input placeholder="Confirmer le mot de passe" type="password" id="password2" name="password2" required class="form-control">
         </div>
         <div class="d-flex justify-content-center align-items-center">
-            <input type="checkbox" name="agree" required class="form-check-input"> &nbsp;
-            J'accepte les <a href="conditions.php" target="_blank">conditions d'utilisation</a>.
+            <label>
+                <input type="checkbox" name="agree" required class="form-check-input"> &nbsp;
+                J'accepte les <a href="conditions.php" target="_blank">conditions d'utilisation</a>.
+            </label>
         </div>
         <div class="btn-group d-flex justify-content-around">
             <a href="connexion.php" type="button" class="btn btn-primary btn-lg">Se connecter</a>
