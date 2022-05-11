66e7dfa19ebcba3a11d49f8dee472013c4260179
Mamadou Moustapha Diallo <sumptring@gmail.com>
2022-05-11 10:13:37 +0000

    Designing the subscription page & adding some php

--- inscription.php
+++ inscription.php
@@ -1,7 +1,46 @@
-<?php 
-$fichier_style="css/inscription.css";
-require "includes/header.php" ; ?>
-<div class="main d-flex flex-column align-items-center">
+<?php
+require "config.php";
+if(isset($_POST['submit'])){
+	$name = htmlentities($_POST['name']);
+	$username = htmlentities($_POST['username']);
+	$password1 = htmlentities($_POST['password1']);
+	$password2 = htmlentities($_POST['password2']);
+	$email = htmlentities($_POST['email']);
+
+	if($username !== '' && $name !== '' && $password1 !== '' && $password2 !== "" && $email !== ""){
+		$db = new mysqli($hname, $uname, $pword, $dbase);
+
+		if($password1 === $password2){
+			$stmt = $db->prepare("SELECT * FROM users WHERE username = ?");
+			$stmt->bind_param('s', $username);
+			$stmt->execute();
+			$resultuser = $stmt->get_result();
+
+			if($resultuser->num_rows === 0){
+				$stmt = $db->prepare("SELECT * FROM users WHERE email = ?");
+				$stmt->bind_param('s', $email);
+				$stmt->execute();
+				$resultuser = $stmt->get_result();
+
+				if($resultuser->num_rows === 0){
+					$stmt = $db->prepare("INSERT INTO users(name, username, password, email, role) VALUES(?, ?, ?, ?, ?)");
+					$stmt->bind_param('sssss', $name, $username, $password1, $email, USER);
+					$stmt->execute();
+					
+					
+				}
+			}
+
+		}
+	}	
+	else{
+
+	}
+}
+else{
+	$fichier_style="css/inscription.css";
+	require "includes/header.php" ; ?>
+	<div class="main d-flex flex-column align-items-center">
     <form action="" method="post" class="form">
         <h2 class="text-center">Inscription</h2>
         <div class="input-group">
@@ -27,8 +66,10 @@ require "includes/header.php" ; ?>
         </div>
         <div class="btn-group d-flex justify-content-around">
             <a href="connexion.php" type="button" class="btn btn-primary btn-lg">Se connecter</a>
-            <button type="submit" class="btn btn-primary btn-lg">S'inscrire</button>
+            <button type="submit" class="register-user btn btn-primary btn-lg" name="submit" value="submit">S'inscrire</button>
         </div>
     </form>
 </div>
-<?php require "includes/footer.php" ; ?>
+<?php
+    require "includes/footer.php" ;
+} ?>
