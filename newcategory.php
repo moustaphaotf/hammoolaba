d895ed02b12e5cdd13bde029d18e55b446925c1a
Mamadou Moustapha Diallo <sumptring@gmail.com>
2022-05-13 14:27:46 +0000

    Fixed : nouvelle catégorie
    Taille de police
    mise en garde sessionnaire

--- newcategory.php
+++ newcategory.php
@@ -1,6 +1,8 @@
 <?php 
 require 'config.php';
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+  session_start();
+}
 
 if(!isset($_SESSION['USER_ID'])){
   header("Location:connexion.php");
