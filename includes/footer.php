d670b45e21f3b35a30dd62062761646bfcd65b08
Mamadou Moustapha Diallo <moustaphaotf@gmail.com>
2022-05-20 23:20:09 +0000

    Mise en forme du footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,12 +1,50 @@
      </div> <!--fin de la page-->
     
-     <footer>
-        <div class="container-fluid mt-3">
-            <div class="card">
-                <div class="row g-0">
-                    <div class="col-md-8 border-right">
-                        <div class="d-flex justify-content-between p-4"> <span class="text-white">2021 Copyright</span> <span class="text-white">Terms and conditions</span> </div>
+     <footer class="card">
+        <div class="container mt-3">
+            <div class="text-light">
+                <!-- Les Liens exterrnes-->
+                <div class="d-flex justify-content-center pb-3">
+                    <div class="d-flex justify-content-evenly w-25 lien-externes py-2">
+                        <a href="http://www.facebook.com" target="blank"><i class="fa fa-facebook"></i></a>
+                        <a href="http://www.twitter.com" target="blank"><i class="fa fa-twitter"></i></a>
+                        <a href="http://instagram.com" target="blank"><i class="fa fa-instagram"></i></a>
+                        <a href="http://linkedin.com" target="blank"><i class="fa fa-linkedin"></i></a>
+                        <a href="http://google.com" target="blank"><i class="fa fa-google"></i></a>
                     </div>
+                </div>
+                
+                <!-- Les trucs du milieu -->
+                <div class="row">
+                    <div class="col-sm-6 col-lg-3  pt-4 pt-lg-0 d-flex flex-column align-items-center">
+                        <h3>Adresse</h3>
+                        <div><i class="fa fa-map-marker"></i></div>
+                        <div>Guinée, Mamou, Télico</div>
+                    </div>
+                    
+                    <div class="col-sm-6 col-lg-3  pt-4 pt-lg-0 d-flex flex-column align-items-center">
+                        <h3>Contacts</h3>
+                        <div>+224 621 000 000</div>
+                        <div>hammoolaba@gmail.com</div>
+                    </div>
+                    
+                    <div class="col-sm-6 col-lg-3  pt-4 pt-lg-0 d-flex flex-column align-items-center">
+                        <h3>Partenaires</h3>
+                        <div>Espace TV</div>
+                        <div>Guinée matin</div>
+                    </div>
+
+                    <div class="col-sm-6 col-lg-3 pt-4 pt-lg-0 d-flex flex-column align-items-center">
+                        <h3>A propos de nous</h3>
+                        <div><i class="fa fa-suitcase"></i></div>
+                        <div class="read-more"><a href="about.php" class="btn">Lire plus <i class="fa fa-arrow-alt-circle-right"></i></a></div>
+                    </div>
+                </div>
+
+                <!-- le copyright-->
+                <hr>
+                <div class="d-flex justify-content-center mt-2">
+                    <p class="m-0">Hammoolaba &copy; Mai 2022</p>
                 </div>
             </div>
         </div>
